f9a4f48f146371f4e85346cb3e879fd94ddb98b3
Maike Castilho <83382149+maikeCastilho@users.noreply.github.com>
2024-05-17 17:16:34 -0300

    first commit

---
+++ Model/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Financeiro.Model
+{
+    internal class Account
+    {
+        private long number;
+        private decimal balance;
+
+        public long Number
+        {
+            get { return number; }
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public Account(long number)
+        {
+            this.number = number;
+        }
+
+        public Account(long number, decimal balance)
+        {
+            this.number = number;
+            this.balance = balance;
+        }
+
+        public void deposit(decimal value)
+        {
+            if (value > 0)
+            {
+                balance += value;
+            }
+        }
+
+        public void withdraw(decimal value)
+        {
+            if (value > 0 && balance > 0)
+            {
+                balance -= value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Sistema_Financeiro.Model;
+
+namespace Sistema_Financeiro 
+{ 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(123);
+            account.deposit(500);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+
+}
